26bacf47b159646e6c1c7ff326031278e1392db6
Michael Blackmon <miblackmon@davidson.edu>
2019-11-25 00:34:35 -0500

    update for presentation

--- jdg/notebooks/trial.py
+++ jdg/notebooks/trial.py
@@ -8,84 +8,84 @@ from collections import Counter
 
 def polyTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
-	model = SVC(kernel = 'poly', max_iter = 1000000)
-	grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
-	grid.fit(X_train, y_train)
-	print(grid.best_params_)
+    model = SVC(kernel = 'poly', max_iter = 1000000)
+    grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
+    grid.fit(X_train, y_train)
+    print(grid.best_params_)
 
-	model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
-	model.fit(X_train, y_train)
+    model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
+    model.fit(X_train, y_train)
 
-	print(" .. Done")
+    print(" .. Done")
 
-	predictions = model.predict(X_train)
-	print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
+    predictions = model.predict(X_train)
+    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
 
-	predictions = model.predict(X_test)
-	print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
+    predictions = model.predict(X_test)
+    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
 
-	print(classification_report(y_test, predictions, target_names=target_names))
+    print(classification_report(y_test, predictions, target_names=target_names))
+    pass
 
 def rbfTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
-	model = SVC(kernel = 'rbf', max_iter = 1000000)
-	grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
-	grid.fit(X_train, y_train)
-	print(grid.best_params_)
+    model = SVC(kernel = 'rbf', 
+                max_iter = 1000000)
+    
+    grid = GridSearchCV(model, param_grid, 
+                        verbose=3, n_jobs=-1, cv=5)
+    grid.fit(X_train, y_train)
+    
+    print(grid.best_params_)
 
-	model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
-	model.fit(X_train, y_train)
+    model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
+    model.fit(X_train, y_train)
 
-	print(" .. Done")
+    print(" .. Done")
+    predictions = model.predict(X_train)
 
-	predictions = model.predict(X_train)
-	print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
-
-	predictions = model.predict(X_test)
-	print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
-
-	print(classification_report(y_test, predictions, target_names=target_names))
-	
+    print(classification_report(y_test, predictions, target_names=target_names))
+    pass
 
 def linearTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
-	model = SVC(kernel = 'linear', max_iter = 1000000)
-	grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
-	grid.fit(X_train, y_train)
-	print(grid.best_params_)
+    model = SVC(kernel = 'linear', max_iter = 1000000)
+    grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
+    grid.fit(X_train, y_train)
+    print(grid.best_params_)
 
-	model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
-	model.fit(X_train, y_train)
+    model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
+    model.fit(X_train, y_train)
 
-	print(" .. Done")
+    print(" .. Done")
 
-	predictions = model.predict(X_train)
-	print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
+    predictions = model.predict(X_train)
+    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
 
-	predictions = model.predict(X_test)
-	print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
+    predictions = model.predict(X_test)
+    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
 
-	print(classification_report(y_test, predictions, target_names=target_names))
-	
-	
+    print(classification_report(y_test, predictions, target_names=target_names))
+    pass
+
 def linearSVCTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
-	model = LinearSVC(max_iter = 1000000)
-	grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
-	grid.fit(X_train, y_train)
-	print(grid.best_params_)
+    model = LinearSVC(max_iter = 1000000)
+    grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
+    grid.fit(X_train, y_train)
+    print(grid.best_params_)
 
-	model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
-	model.fit(X_train, y_train)
+    model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
+    model.fit(X_train, y_train)
 
-	print(" .. Done")
+    print(" .. Done")
 
-	predictions = model.predict(X_train)
-	print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
+    predictions = model.predict(X_train)
+    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
 
-	predictions = model.predict(X_test)
-	print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
+    predictions = model.predict(X_test)
+    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
 
-	print(classification_report(y_test, predictions, target_names=target_names))
-	
-	
+    print(classification_report(y_test, predictions, target_names=target_names))
+    pass
+    
