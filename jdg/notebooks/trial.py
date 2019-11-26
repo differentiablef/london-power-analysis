65cfdc0ef2027d3ffdcbcb2f0b7b41e30dde5f1d
Michael Blackmon <miblackmon@davidson.edu>
2019-11-25 20:49:58 -0500

    clean up for pres.

--- jdg/notebooks/trial.py
+++ jdg/notebooks/trial.py
@@ -15,40 +15,37 @@ def polyTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
     model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
     model.fit(X_train, y_train)
-
     print(" .. Done")
-
+    
     predictions = model.predict(X_train)
-    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
+    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / 
+                                     sum(Counter(y_train-predictions).values()),2))
 
     predictions = model.predict(X_test)
-    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
+    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / 
+                                     sum(Counter(y_test-predictions).values()),2))
 
     print(classification_report(y_test, predictions, target_names=target_names))
     pass
 
 def rbfTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
-
     model = SVC(kernel = 'rbf', 
                 max_iter = 1000000)
     
     grid = GridSearchCV(model, param_grid, 
                         verbose=3, n_jobs=-1, cv=5)
     grid.fit(X_train, y_train)
-    
     print(grid.best_params_)
 
     model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
     model.fit(X_train, y_train)
-
     print(" .. Done")
+    
     predictions = model.predict(X_train)
-
     print(classification_report(y_test, predictions, target_names=target_names))
     pass
 
 def linearTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
-
     model = SVC(kernel = 'linear', max_iter = 1000000)
     grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
     grid.fit(X_train, y_train)
@@ -56,20 +53,20 @@ def linearTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
     model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
     model.fit(X_train, y_train)
-
     print(" .. Done")
 
     predictions = model.predict(X_train)
-    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
+    print("Train accuracy = ", round(Counter(y_train-predictions)[0] /
+                                     sum(Counter(y_train-predictions).values()),2))
 
     predictions = model.predict(X_test)
-    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
+    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] /
+                                     sum(Counter(y_test-predictions).values()),2))
 
     print(classification_report(y_test, predictions, target_names=target_names))
     pass
 
 def linearSVCTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
-
     model = LinearSVC(max_iter = 1000000)
     grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=-1, cv=5)
     grid.fit(X_train, y_train)
@@ -77,14 +74,15 @@ def linearSVCTrial(param_grid, target_names, X_train, X_test, y_train, y_test):
 
     model = SVC(verbose=3, kernel = 'rbf', max_iter = 1000000, **grid.best_params_)
     model.fit(X_train, y_train)
-
     print(" .. Done")
 
     predictions = model.predict(X_train)
-    print("Train accuracy = ", round(Counter(y_train-predictions)[0] / sum(Counter(y_train-predictions).values()),2))
+    print("Train accuracy = ", round(Counter(y_train-predictions)[0] /
+                                     sum(Counter(y_train-predictions).values()),2))
 
     predictions = model.predict(X_test)
-    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] / sum(Counter(y_test-predictions).values()),2))
+    print("Test accuracy  = ", round(Counter(y_test-predictions)[0] /
+                                     sum(Counter(y_test-predictions).values()),2))
 
     print(classification_report(y_test, predictions, target_names=target_names))
     pass
